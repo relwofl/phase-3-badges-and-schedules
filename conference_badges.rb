038d3bd111e54950a9b1eab189caae3466d02e5b
Luke <luciusfow@gmail.com>
2022-06-12 15:37:52 -0400

    commit tree 42237df91d8753439c344d02c2062162e8887cb1

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+require 'pry'
+def badge_maker(name)
+     "Hello, my name is #{name}."
+end
+def batch_badge_creator(attendees)
+    attendees.map do |attendee|
+        "Hello, my name is #{attendee}."
+    end 
+end
+
+def assign_rooms(attendees)
+    attendees.map.with_index(1) do |attendee, room_num|
+        "Hello, #{attendee}! You'll be assigned to room #{room_num}!"
+    end 
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+        puts badge
+    end
+    assign_rooms(attendees).each do |room_num|
+        puts room_num
+        binding.pry
+    
+    end
+end
